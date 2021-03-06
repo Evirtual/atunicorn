1db6a20baf63671c52a29aba8cd61282536ffe73
egislook <noneedsystem@gmail.com>
2021-03-06 13:53:53 +0700

    feat: added fontawesome

--- @packs/pack/elems/index.js
+++ @packs/pack/elems/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Actheme } from '../theme'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Svg from 'react-native-svg'
+// import * as All from './../resources/svg'
 
 export const Link = ({ href, prefetch, replace, scroll, shallow, assetPrefix = process.env.assetPrefix, ...rest }) => (
 	<Styled.Link as={`${assetPrefix || ''}${href}`} href={href} passHref={true} prefetch={prefetch} replace={replace} scroll={scroll} shallow={shallow}>
@@ -12,6 +15,9 @@ const Styled = Actheme.create({
 	Link: 'Link'
 })
 
+const Icon = ({ svg, ...props }) => <FontAwesomeIcon {...props} alt={props.alt} icon={[ (props.solid ? 'fas' : 'fal'), props.icon]} solid={(props.solid?.toString())} />
+
 export default {
-	Link
+	Link,
+	Icon
 }
--- web/pages/_app.js
+++ web/pages/_app.js
@@ -3,6 +3,19 @@ import Actstore from 'actstore'
 import Head from 'next/head'
 import Settings from 'pack/store'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/pro-solid-svg-icons'
+import { fal } from '@fortawesome/pro-light-svg-icons'
+
+library.add(fas, fal)
+
+/** Install pro
+  npm config set "@fortawesome:registry" https://npm.fontawesome.com/
+  npm config set "//npm.fontawesome.com/:_authToken" 2C14ED20-7F10-4AB4-9C27-184BDD7CBFD8
+*/
+
 const App = ({ Component, pageProps }) => {
 	const { store, act } = Actstore(Settings, ['ready'])
 	React.useEffect(() => { act('APP_INIT') }, [])
