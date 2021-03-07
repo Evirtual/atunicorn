92363d845f7b2d342dfd9e0645d5780cdf2c963c
Living <3dgars@gmail.com>
2021-03-07 21:48:22 +0700

    modified main screen and spinner applied

--- @packs/pack/screens/main.js
+++ @packs/pack/screens/main.js
@@ -8,19 +8,23 @@ function MainScreen(props) {
   const { user, posts, } = store.get('user', 'posts')
 
   console.log({ user, posts })
-  if(!posts) return null
+  // if(!posts) return null
 
   return <Inner.Container>
     <Inner.Wrap>
       <Button.Comp source="https://techcrunch.com/wp-content/uploads/2018/07/logo-2.png" text="Logo" logo size="s20" />
-      <Inner.Tabs>
-        <Button.Comp tab text="Images" />
-        <Button.Comp tab text="Videos" />
-      </Inner.Tabs>
+      <Inner.Menu>
+        <Inner.Tabs>
+          <Button.Comp tab text="Images" />
+          <Button.Comp tab text="Videos" />
+        </Inner.Tabs>
+        <Inner.Content>
+          {!posts
+            ? <Button.Comp icon="spinner-third" iconColor="black" iconSize="s10" spin />
+            : posts.map((post, index) => <Post.Comp key={index} post={post} />)}
+        </Inner.Content>
+      </Inner.Menu>
     </Inner.Wrap>
-    <Inner.Content>
-      {!!posts && posts.map((post, index) => <Post.Comp key={index} post={post} />)}
-    </Inner.Content>
     <Inner.Footer>
       <Button.Comp icon="info-circle" iconColor="black" iconSize="s10" info onPress={action('APP_LOGIN')} />
     </Inner.Footer>
@@ -30,20 +34,21 @@ export default MainScreen
 
 const Inner = Actheme.create({
   Container: ['View', 'f:1 bg:black50'],
-  Wrap: ['View', 'w:100% ai:c p:s5', { md: 'fd:row jc:sb' }],
-  Tabs: ['View', 'fd:row jc:c mt:s5', { md: 'mt:0' }],
+  Wrap: ['View', 'f:1 w:100% ai:c', { md: 'fd:row ai:fs' }],
+  Menu: ['View', 'f:1 w,h:100%'],
+  Tabs: ['View', 'fd:row w:100% jc:c', { md: 'p:s5' }],
   Content: ['ScrollView', ['f:1', { contentContainerStyle: Actheme.style('fg:1 jc:c fd:row fw:wrap ph:s5') }]],
   Footer: ['View', 'ps:ab b,l:0 p:s5 z:2']
 })
 
 const Button = Actheme.create({
-  Touch: ['TouchableOpacity', 'jc,ai:c', { tab: 'mh:s5 p:s5 fs:s20', info: 'bg:#e5e5e5 br:s20' }],
+  Touch: ['TouchableOpacity', 'jc,ai:c', { tab: 'mh:s5 p:s5 fs:s20', info: 'bg:#e5e5e5 br:s20', logo: 'm:s5' }],
   Text: ['Text', ['ta:c c:black fb:500 w:100%', { numberOfLines: 1 }], { tab: 'fs:s5' }],
   Image: ['Image', 'w,h:100%'],
-  Comp: ({text, source, info, logo, size, icon, iconColor, iconSize, tab, ...props}) => {
-    return <Button.Touch info={info} logo={logo} tab={tab} style={Actheme.style(`w,h:${size || 'auto'}`)} {...props}>
+  Comp: ({text, source, info, logo, size, icon, iconColor, iconSize, spin, tab, ...props}) => {
+    return <Button.Touch info={info} logo={logo} tab={tab} logo={logo} style={Actheme.style(`w,h:${size || 'auto'}`)} {...props}>
       {source && <Button.Image source={source} />}
-      {icon && <Elems.Icon color={Actheme.value(iconColor, 'color')} style={Actheme.style(`fs:${iconSize}`)} icon={icon} />}
+      {icon && <Elems.Icon color={Actheme.value(iconColor, 'color')} style={Actheme.style(`fs:${iconSize}`)} icon={icon} spin={spin} />}
       {text && <Button.Text tab={tab}>{text}</Button.Text>}
     </Button.Touch>
   }
