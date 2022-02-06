72436f2c93450e2d1e867ac1b3a471e39689ba9d
Living <3dgars@gmail.com>
2022-02-06 23:33:47 +0700

    updated logo url to firebase

--- @packs/pack/comps/nav/index.js
+++ @packs/pack/comps/nav/index.js
@@ -85,7 +85,7 @@ const Nav = Actheme.create({
                 </Nav.File>
               : path !== '/' && path !== '/about' && !profile.url
                 ? <Elems.Icon style={Actheme.style('c:black100 fs:s20')} icon="user-circle" solid />
-                : <Nav.Image source={profile.url ? profile.url : 'https://firebasestorage.googleapis.com/v0/b/unicorn-ee877.appspot.com/o/uni-latest-slow.gif?alt=media&token=d14b886d-2512-4c1b-a04b-c69601d769ba' } />
+                : <Nav.Image source={profile.url ? profile.url : 'https://firebasestorage.googleapis.com/v0/b/unicorn-ee877.appspot.com/o/unicorn-io.gif?alt=media&token=5612f449-9fe6-45f7-aac2-0380def4c444' } />
             }
           </Nav.Wrap>
           <Elems.Button text="about" onPress={path === '/' ? () => router.push('/about/') : () => router.push('/profile/' + id + '/about/')} />
