5151d4e67dbee9348bdea98914c12fb9445abb52
Living <3dgars@gmail.com>
2022-03-01 07:39:36 +0700

    updated

--- @packs/pack/screens/main.js
+++ @packs/pack/screens/main.js
@@ -35,6 +35,6 @@ export default MainScreen
 
 const Styled = Actheme.create({
   Container: ['View', 'f:1 bg:black25'],
-  Content: ['ScrollView', ['f:1', { contentContainerStyle: Actheme.style('jc:c fd:row fw:wrap w:100% xw:s400 as:c ph:s5'), showsVerticalScrollIndicator: true }]],
+  Content: ['ScrollView', ['f:1', { contentContainerStyle: Actheme.style('jc:c fd:row fw:wrap w:100% xw:s400 as:c ph:s5 pb:s5'), showsVerticalScrollIndicator: true }]],
   Wrap: ['View', 'w:100% ai:c']
 })
