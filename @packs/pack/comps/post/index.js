7c0309344474747ea1568f3f93eeb1acd1eb5223
Living <3dgars@gmail.com>
2022-03-29 21:50:58 +0700

    updated post

--- @packs/pack/comps/post/index.js
+++ @packs/pack/comps/post/index.js
@@ -10,10 +10,10 @@ const Post = Actheme.create({
   Content: ['View', 'w,h:90vw xw,xh:s90 br:s5 of:hd bg:white bw:1 bc:black50'],
   Wrap: ['View', 'ps:ab t,l:s2 z:3 fd:row ai:c'],
   Image: ['Image', 'w,h:100% br:s5'],
-  Profile: ['TouchableOpacity', 'w,h,br:s12 of:hd bg:black200 bw:2 bc:white ai,jc:c'],
-  User: ['View', 'ml:s2 bg:white400 bw:2 bc:black pv:s2 ph:s3 br:s3'],
+  Profile: ['TouchableOpacity', 'w,h,br:s12 of:hd bg:black200 bw:2 bc:white ai,jc:c z:2'],
+  User: ['View', 'ml:-s5 bg:white400 pv:s2.5 pr:s3 pl:s6 br:s3'],
   Name: ['Text', 'c:black fb:500'],
-  Options: ['View', 'fd:row ps:ab t,r:s1.5 ai,jc:c z:3'],
+  Options: ['View', 'fd:row ps:ab t,r:s2 ai,jc:c z:3'],
   Cover: ['TouchableOpacity', 'ps:ab z:2 t,b,l,r:0'],
 
   Comp: (props) => {
@@ -27,35 +27,6 @@ const Post = Actheme.create({
 
     return (
       <Post.Container>
-        {user && user?.id === id &&
-          <Post.Options>
-            <Elems.Button
-              option
-              edit
-              regular
-              icon="pencil"
-              onPress={onEdit}
-              style={Actheme.style('mr:s1')} />
-            <Elems.Button
-              option
-              recycle
-              regular
-              icon="recycle"
-              onPress={
-                () => 
-                  act('APP_DELETEPOST', { userId: user?.id , postId: post?.id , url: post?.url })
-                    .then(onRemove, setRecycling(true))
-              }
-            />
-          </Post.Options>
-        }
-        {post.nsfw && !nsfw &&
-          <Post.Cover onPress={() => setNsfw(true)}>
-            <Placeholder
-              icon="eye-slash"
-              title="NSFW" />
-          </Post.Cover>
-        }
         {!id &&
           <Post.Wrap>
             <Post.Profile onPress={() => setActive(!active)}>
@@ -92,6 +63,35 @@ const Post = Actheme.create({
             }
           </Post.Content>
         </Elems.Link>
+        {user && user?.id === id &&
+          <Post.Options>
+            <Elems.Button
+              option
+              edit
+              regular
+              icon="pencil"
+              onPress={onEdit}
+              style={Actheme.style('mr:s1')} />
+            <Elems.Button
+              option
+              recycle
+              regular
+              icon="recycle"
+              onPress={
+                () => 
+                  act('APP_DELETEPOST', { userId: user?.id , postId: post?.id , url: post?.url })
+                    .then(onRemove, setRecycling(true))
+              }
+            />
+          </Post.Options>
+        }
+        {post.nsfw && !nsfw &&
+          <Post.Cover onPress={() => setNsfw(true)}>
+            <Placeholder
+              icon="eye-slash"
+              title="NSFW" />
+          </Post.Cover>
+        }
       </Post.Container>
     )
   }
--- @packs/pack/screens/post.js
+++ @packs/pack/screens/post.js
@@ -53,7 +53,7 @@ export default function PostScreen() {
                       regular
                       icon="pencil"
                       onPress={() => setEdit(true)}
-                      style={Actheme.style('mr:s1')} />
+                      style={Actheme.style('mr:s2')} />
                     <Elems.Button
                       option
                       recycle
@@ -104,5 +104,5 @@ const Post = Actheme.create({
   Text: ['Text', 'fs:s4 p:s5',],
   Profile: ['View', 'w:100% fd:row ai:c p:s5'],
   Name: ['Text', 'fs:s4 fb:500 ml:s2'],
-  Options: ['View', 'fd:row ps:ab t,r:s1.5 ai,jc:c z:3'],
+  Options: ['View', 'fd:row ps:ab t,r:s2 ai,jc:c z:3'],
 })
