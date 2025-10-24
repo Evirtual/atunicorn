93cc82345f29be5ac9554f8c6c3ab220dd802339
Edgaras Neverdauskas <you@example.com>
2025-10-24 14:43:19 +0700

    updated

--- web/pages/post/[id].js
+++ web/pages/post/[id].js
@@ -1,1 +1,31 @@
-export { default } from 'pack/screens/post'
+import React, { useState } from 'react'
+import PostScreen from 'pack/screens/post'
+import { useStoreContext } from 'pack/store'
+
+const PostPage = () => {
+	const { state, act, handle } = useStoreContext()
+	const { user, users, posts } = state || {}
+
+	const router = handle.useRouter()
+	const { id } = router?.query || {}
+
+	const [mode, setMode] = useState(false)
+	const [profileId, setProfileId] = useState(null)
+
+	return (
+		<PostScreen
+			act={act}
+			user={user}
+			users={users}
+			posts={posts}
+			router={router}
+			id={id}
+			mode={mode}
+			setMode={setMode}
+			profileId={profileId}
+			setProfileId={setProfileId}
+		/>
+	)
+}
+
+export default PostPage
