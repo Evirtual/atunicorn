8b3184cd8c73432000cf8977a9552089c5b79045
Edgaras Neverdauskas <you@example.com>
2025-10-26 16:58:42 +0700

    updated

--- @packs/pack/elems/icon/index.js
+++ @packs/pack/elems/icon/index.js
@@ -1,30 +1,95 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as Icons from '@phosphor-icons/react'
 import { Actheme } from '../../theme'
 
-const Icon = (props) => {
+const ICON_ALIASES = {
+  'arrow-circle-left': 'ArrowCircleLeft',
+  'arrow-circle-up': 'ArrowCircleUp',
+  camera: 'Camera',
+  'eye-slash': 'EyeSlash',
+  home: 'House',
+  'image-polaroid': 'ImageSquare',
+  'info-circle': 'Info',
+  pencil: 'Pencil',
+  'plus-circle': 'PlusCircle',
+  'power-off': 'Power',
+  recycle: 'Recycle',
+  save: 'FloppyDisk',
+  search: 'MagnifyingGlass',
+  times: 'X',
+  'times-circle': 'XCircle',
+  'user-circle': 'UserCircle',
+  'yin-yang': 'YinYang'
+}
 
-  const { svg, onLayout, alt, regular, solid, brands, icon, name, iconColor, iconSize, ...rest } = props
+const toComponentName = value => {
+  if (!value) return null
+  if (ICON_ALIASES[value]) return ICON_ALIASES[value]
+  return value
+    .split('-')
+    .filter(Boolean)
+    .map(segment => segment.charAt(0).toUpperCase() + segment.slice(1))
+    .join('')
+}
+
+const FallbackIcon = Icons.Question || Icons.Circle || Icons.Smiley
+
+const Icon = props => {
+  const {
+    alt,
+    brands,
+    className,
+    icon,
+    iconColor,
+    iconSize,
+    name,
+    regular,
+    solid,
+    spin,
+    style,
+    weight,
+    ...rest
+  } = props
+
+  const iconKey = name || icon
+  const componentName = toComponentName(iconKey)
+  const IconComponent = (componentName && Icons[componentName]) || FallbackIcon
+
+  const tokens = [
+    iconSize && `fs:${iconSize}`,
+    iconColor && `c:${iconColor}`
+  ].filter(Boolean).join(' ')
+
+  const themedStyle = tokens ? Actheme.style(tokens) : {}
+  const { color, fontSize, ...styleRest } = themedStyle || {}
+
+  const resolvedWeight = weight
+    || (solid ? 'fill' : regular ? 'regular' : brands ? 'regular' : 'regular')
+
+  const composedClassName = [className, spin && 'ac-icon-spin']
+    .filter(Boolean)
+    .join(' ') || undefined
+
+  const inlineStyle = style && typeof style === 'object' ? style : undefined
+  const composedStyle = {
+    ...styleRest,
+    ...(inlineStyle || {})
+  }
+
+  const cleanedProps = { ...rest }
+  delete cleanedProps.svg
+  delete cleanedProps.onLayout
 
   return (
-    <FontAwesomeIcon
-      alt={alt}
-      icon={[(
-        regular
-          ? 'far'
-          : brands
-            ? 'fab'
-            : solid
-              ? 'fas'
-              : 'fal'),
-        icon || name]}
-      regular={(regular?.toString())}
-      solid={(solid?.toString())}
-      style={Actheme.style([
-        iconSize && `fs:${iconSize}`,
-        iconColor && `c:${iconColor}`
-      ].filter(item => item).join(' '))}
-      {...rest} />
+    <IconComponent
+      aria-label={alt}
+      className={composedClassName}
+      color={cleanedProps.color ?? color}
+      size={cleanedProps.size ?? fontSize}
+      style={Object.keys(composedStyle).length ? composedStyle : undefined}
+      weight={resolvedWeight}
+      {...cleanedProps}
+    />
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,7 @@ import Actstore from 'actstore'
 import Settings from 'pack/store'
 import Layout from 'pack/comps/layout'
 
-import '@fortawesome/fontawesome-svg-core/styles.css'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-library.add(fal, far, fad, fas, fab)
-
-/** Install pro
-  npm config set "@fortawesome:registry" https://npm.fontawesome.com/
-  npm config set "//npm.fontawesome.com/:_authToken" 2C14ED20-7F10-4AB4-9C27-184BDD7CBFD8
-*/
+import '../styles/icons.css'
 
 const App = ({ Component, pageProps }) => {
 
