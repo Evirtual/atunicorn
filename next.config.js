5f0e6bef538ac21b9e14c857c2b73350a2f59975
Edgaras Neverdauskas <you@example.com>
2025-10-27 17:17:25 +0700

    updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 const withTM = require('next-transpile-modules')(['pack'])
 
-const { NODE_ENV, __CFBundleIdentifier, __CF_USER_TEXT_ENCODING, __NEXT_PROCESSED_ENV, NODE_EXE, ...env } = process.env
+const IGNORED_ENV_KEYS = new Set([
+  'NODE_ENV',
+  '__CFBundleIdentifier',
+  '__CF_USER_TEXT_ENCODING',
+  '__NEXT_PROCESSED_ENV',
+  'NODE_EXE'
+])
+
+const filteredEnv = Object.entries(process.env).reduce((acc, [key, value]) => {
+  if (IGNORED_ENV_KEYS.has(key)) return acc
+  if (key.startsWith('NEXT_')) return acc
+  acc[key] = value
+  return acc
+}, {})
 
 module.exports = withTM({
   trailingSlash: true,
-  env: { ...env, ENV: NODE_ENV },
+  env: { ...filteredEnv, ENV: process.env.NODE_ENV },
   webpack: config => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
